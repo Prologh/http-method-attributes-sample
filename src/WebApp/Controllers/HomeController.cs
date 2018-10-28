d307c244e964a738019fd29c4193dc2a579e06d6
Piotr Wosiek <piotrwosiek@onet.eu>
2018-10-28 12:18:54 +0100

    Add simplified web application

---
+++ src/WebApp/Controllers/HomeController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Net.Http;
+
+namespace WebApp.Properties.Controllers
+{
+    public class HomeController : Controller
+    {
+        [HttpGet][HttpPost]
+        public IActionResult Index()    // <== Action supporting more than one type of HTTP request.
+        {
+            // GET
+            if (Request.Method.Equals(HttpMethod.Get.Method))
+            {
+                return View();
+            }
+
+            // POST
+            if (Request.Method.Equals(HttpMethod.Post.Method))
+            {
+                return View();
+            }
+
+            return NotFound();
+        }
+    }
+}
